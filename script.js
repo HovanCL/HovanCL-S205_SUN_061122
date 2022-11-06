0c216853580c0436fc7467666d0ec219aa55b613
HovanTan <hovan.tan@codinglab.com.sg>
2022-11-06 10:46:34 +0800

    Did some CSS and JS

---
+++ script.js
@@ -0,0 +1,7 @@
+var num = 1;
+var username = window.prompt("What is your name?");
+var age = window.prompt("What is your age?");
+
+document.getElementById("num").innerHTML = num;
+document.getElementById("username").innerHTML = username;
+document.getElementById("age").innerHTML = age;
